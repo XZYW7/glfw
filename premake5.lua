d8b8a8fb5f59b5d5da17b724059ad8464d6734bf
XZYW <928144597@qq.com>
2023-07-09 00:59:17 +0800

    convert shared lib to static lib

--- premake5.lua
+++ premake5.lua
@@ -31,7 +31,6 @@ project "GLFW"
     }
     
 	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
         staticruntime "On"
         
@@ -58,5 +57,9 @@ project "GLFW"
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
 		}
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "on"
